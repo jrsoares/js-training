a6add1134025a9844d136209000d0b9649ddce93
Jr.Soares <juniorsoaresti@gmail.com>
2022-06-30 19:45:55 -0400

    Accordion List

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const listaAnimais = document.querySelectorAll('.js-lista-animais li');
+function initTab() {
+  const listaAnimais = document.querySelectorAll('.js-lista-animais li');
 
-const descricaoAnimal = document.querySelectorAll(
-  '.js-descricao-animal section'
-);
+  const descricaoAnimal = document.querySelectorAll(
+    '.js-descricao-animal section'
+  );
 
-descricaoAnimal[0].classList.add('ativo');
+  descricaoAnimal[0].classList.add('ativo');
 
-function handleTabImageClick(index) {
-  descricaoAnimal.forEach((item) => {
-    item.classList.remove('ativo');
+  function handleTabImageClick(index) {
+    descricaoAnimal.forEach((item) => {
+      item.classList.remove('ativo');
+    });
+    descricaoAnimal[index].classList.add('ativo');
+  }
+
+  listaAnimais.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      handleTabImageClick(index);
+    });
   });
-  descricaoAnimal[index].classList.add('ativo');
+}
+initTab();
+
+const faqList = document.querySelectorAll('.js-faq-lista dt');
+const classActive = 'ativo';
+
+function handleFaqClick() {
+  this.classList.toggle(classActive);
+  this.nextElementSibling.classList.toggle(classActive);
 }
 
-listaAnimais.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    handleTabImageClick(index);
-  });
+faqList.forEach((item) => {
+  item.addEventListener('click', handleFaqClick);
 });

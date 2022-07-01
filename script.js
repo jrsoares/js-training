aa63fb97e1bf8a1e664eead6876ebc65831203aa
Jr.Soares <juniorsoaresti@gmail.com>
2022-06-30 22:05:11 -0400

    initMenuScroll

--- script.js
+++ script.js
@@ -22,14 +22,37 @@ function initTab() {
 }
 initTab();
 
-const faqList = document.querySelectorAll('.js-faq-lista dt');
-const classActive = 'ativo';
+function initAccordionList() {
+  const faqList = document.querySelectorAll('.js-faq-lista dt');
+  const classActive = 'ativo';
 
-function handleFaqClick() {
-  this.classList.toggle(classActive);
-  this.nextElementSibling.classList.toggle(classActive);
+  function handleFaqClick() {
+    this.classList.toggle(classActive);
+    this.nextElementSibling.classList.toggle(classActive);
+  }
+
+  faqList.forEach((item) => {
+    item.addEventListener('click', handleFaqClick);
+  });
 }
 
-faqList.forEach((item) => {
-  item.addEventListener('click', handleFaqClick);
-});
+initAccordionList();
+
+function initMenuScroll() {
+  const menuScroll = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function handleMenuScroll(event) {
+    const hrefContent = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(hrefContent);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  menuScroll.forEach((item) => {
+    item.addEventListener('click', handleMenuScroll);
+  });
+}
+
+initMenuScroll();

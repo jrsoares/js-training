2ef2e8cb60fbd8815f79cc6fef1ab97af18fcf77
Jr.Soares <juniorsoaresti@gmail.com>
2022-07-04 21:48:21 -0400

    styling the modal

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import initTab from '../modules/initTab.js';
+import initAccordionList from '../modules/initAccordionList.js';
+import initMenuScroll from '../modules/initMenuScroll.js';
+import initAnimaSection from '../modules/initAnimaSection.js';
+
+initTab();
+
+initAccordionList();
+
+initMenuScroll();
+
+initAnimaSection();
